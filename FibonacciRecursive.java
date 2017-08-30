6449e4e14d9d32cce7ee5f9a3570f58362695793
davidguybrizan <31490363+davidguybrizan@users.noreply.github.com>
2017-08-30 14:06:28 -0700

    Implemented FibonacciRecursive
    Recursive version of Fibonacci implemented. But it wasn't checked. So use at your own peril.

---
+++ FibonacciRecursive.java
@@ -0,0 +1,12 @@
+package edu.usfca.cs.cs245;
+
+
+class FibonacciRecursive implementes Fibonacci {
+
+	public int fibonacci(int n) {
+		if (n == 0 || n == 1) 
+			return n;
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+}
+
